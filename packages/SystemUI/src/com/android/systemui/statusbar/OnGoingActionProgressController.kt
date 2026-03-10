789d8237ddca4e510abbcfba84ae200a9e92d6cf
Pranav Vashi <neobuddy89@gmail.com>
2026-03-10 23:53:20 +0530

    SystemUI: Do not invalidate ongoing chip bg color often
    * We should invalidate / clear it only when user sets it to accent mode.
    * If we invalidate too often, chip background flickers on seekbar manual updates.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/OnGoingActionProgressController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/OnGoingActionProgressController.kt
@@ -227,7 +227,6 @@ class OnGoingActionProgressController(
             currentIcon = context.resources.getDrawable(R.drawable.ic_default_music_icon, context.theme)
         }
         if (chipColorMode == CHIP_COLOR_MODE_ICON && currentIcon != null) {
-            invalidateChipBgColor()
             currentIcon?.let { extractAndApplyChipBgColorFromIcon(it) }
         }
     }
@@ -248,7 +247,6 @@ class OnGoingActionProgressController(
         if (art != null) {
             currentAlbumArt = art
             if (chipColorMode == CHIP_COLOR_MODE_ALBUM_ART) {
-                invalidateChipBgColor()
                 currentAlbumArt?.let { extractAndApplyChipBgColorFromAlbumArt(it) }
             }
         } else if (metadata != null) {  // Partial update, force refresh
@@ -844,11 +842,12 @@ class OnGoingActionProgressController(
         }
 
         if (wasChipColorMode != chipColorMode) {
-            invalidateChipBgColor()
             if (chipColorMode == CHIP_COLOR_MODE_ALBUM_ART && currentAlbumArt != null) {
                 currentAlbumArt?.let { extractAndApplyChipBgColorFromAlbumArt(it) }
             } else if (chipColorMode == CHIP_COLOR_MODE_ICON && currentIcon != null) {
                 currentIcon?.let { extractAndApplyChipBgColorFromIcon(it) }
+            } else {
+                invalidateChipBgColor()
             }
             needsFullUiUpdate = true
         }
